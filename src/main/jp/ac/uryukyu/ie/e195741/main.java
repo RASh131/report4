635d4a0e7baf64e54e04d36547dd223e7fcc82ea
Shoiciro Tomita <e195741@ie.u-ryukyu.ac.jp>
2019-11-26 10:27:50 +0900

    repo4

--- src/main/jp/ac/uryukyu/ie/e195741/main.java
+++ src/main/jp/ac/uryukyu/ie/e195741/main.java
@@ -2,6 +2,12 @@ package jp.ac.uryukyu.ie.e195741;
 public class  main{
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerException発生");
+            System.out.println(e.getMessage());
+        }
     }
 }
