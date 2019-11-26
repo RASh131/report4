9f2cd9cc21ba5534d1555881636c769560981aae
Shoiciro Tomita <e195741@ie.u-ryukyu.ac.jp>
2019-11-26 10:18:25 +0900

    repo4

---
+++ src/main/jp/ac/uryukyu/ie/e195741/main.java
@@ -0,0 +1,10 @@
+package jp.ac.uryukyu.ie.e195741;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class  main{
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
